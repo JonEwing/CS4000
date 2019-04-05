3d511bbdd565fc28fc3ecef7dab5a18db3217d0a
Jonathan Feige <jonfeige@hotmail.com>
2019-04-05 11:21:10 -0400

    Add files via upload

--- hw5/main.cc
+++ hw5/main.cc
@@ -1,172 +1,153 @@
-//******************************************
-//Jonathan Feige
-//CS4900
-//P2.2
-//Takes the files as a path
-//returns a dir or a file
-//******************************************
+//***************************************************************
+// File: Hamiltonian_Path.cc
+// Author: David W. Juedes
+// Edit: Jonathan Feige
+// Purpose: This code finds whether there exists a Hamiltonian Path 
+// from one vertex to another in a graph.
+//****************************************************************
 
-#include <vector>
-#include <string.h>
 #include <iostream>
-#include <fstream>
+#include<fstream>
+#include <vector>
 #include <omp.h>
-#include <unistd.h>
-
+#include <unistd.h> 
+#include <mpi.h>
 using namespace std;
 
-int main(int argc, char *argv[])
-{	
-	vector<string> words;
-	vector<string> cyrps;
-	vector<string> nums;
+
+int main(int argc, char *argv[]) {
+	string file;
+	ifstream infile;
+	int num_processes;
+	int proc_num;
+	vector<string> dictionary;
+	vector<string> passwords;
 	vector<string> combo;
-
-	string in;
-	cout<<"Enter dictionary"<<endl;
-	cin >> in;
-
-	in = "words";
-	string line;
-	ifstream myfile (in);
-	if (myfile.is_open())
+	vector<string> numbers_combo;
+		
+	infile.open (argv[1]);
+        while(!infile.eof()) // To get you all the lines.
+        {
+	        getline(infile,file); // Saves the line in STRING.
+			dictionary.push_back(file);
+        }
+	infile.close();
+	
+	infile.open (argv[2]);
+        while(!infile.eof()) // To get you all the lines.
+        {
+	        getline(infile,file); // Saves the line in STRING.
+			passwords.push_back(file);
+        }
+	infile.close();
+	
+	
+	combo = dictionary; //one word
+	
+	for(int i = 0; i < dictionary.size(); i++) //one word, nums
 	{
-   		while ( getline (myfile,line) )
-		{ 
-			words.push_back(line);
-    	}
-    myfile.close();
+		for(int j = 0; j < dictionary.size(); j++)
+		{
+			combo.push_back(dictionary[i] + dictionary[j]);
+		}
 	}
-
-	cout<<"Enter passwords"<<endl;
-	cin >> in;
-	in = "enc_passwords";
-
- 	ifstream myfile_other (in);
-  	if (myfile_other.is_open())
- 	{
-   		while ( getline (myfile_other,line) )
-		{ 
-			cyrps.push_back(line);
-    	}
-    myfile_other.close();
-	}
-
-	for(int i = 0; i < 10; i++)
+	
+	for(int i = 0; i < 10; i++) //one num
 	{
 		std::string x = std::to_string(i);
-		nums.push_back(x);
+		numbers_combo.push_back(x);
 	}
-
-	for(int i = 0; i < 10; i++)
+	
+	for(int i = 0; i < 10; i++) //two num
 	{
 		for(int j = 0; j < 10; j++)
 		{
 			std::string x = std::to_string(i);
 			std::string y = std::to_string(j);
-			nums.push_back(x + y);
+			numbers_combo.push_back(x + y);
 		}
 	}
-
-	for(int i = 0; i < 10; i++)
+	
+	for(int i = 0; i < 10; i++) //three num
 	{
 		for(int j = 0; j < 10; j++)
-		{	
+		{
 			for(int k = 0; k < 10; k++)
 			{
 				std::string x = std::to_string(i);
 				std::string y = std::to_string(j);
 				std::string z = std::to_string(k);
-				nums.push_back(x + y + z);
+				numbers_combo.push_back(x + y + z);
 			}
 		}
 	}
-
-	combo = words;
-
-	for(int i = 0; i < words.size(); i++) // two words
+	
+	for(int i = 0; i < dictionary.size(); i++) //one word, nums
 	{
-		for(int j = 0; j < words.size(); j++)
+		for(int j = 0; j < numbers_combo.size(); j++)
 		{
-			combo.push_back(words[i] + words[j]);
+			std::string x = std::to_string(j);
+			combo.push_back(dictionary[i] + numbers_combo[j]);
+			combo.push_back(numbers_combo[j] + dictionary[i]);
 		}
 	}
-
-	for(int i = 0; i < words.size(); i++) //one word one num
-	{
-		for(int j = 0; j < nums.size(); j++)
-		{
-			combo.push_back(words[i] + nums[j]);
-			combo.push_back(nums[j] + words[i]);
-		}
-	}
-
-	for(int i = 0; i < words.size(); i++) //one word one num
-	{
-		for(int j = 0; j < words.size(); j++)
-		{	
-			for(int k = 0; k < nums.size(); k++)
-			{
-				combo.push_back(nums[k] + words[i] + words[j]);
-				combo.push_back(words[i] + nums[k] + words[j]);
-				combo.push_back(words[i] + words[j] + nums[k]);
-			}
-		}
-	}
-
-
+	
 	std::string delimiter = "$";
 	vector<string> tok;
-
-
-	for(int i = 0; i < cyrps.size(); i++)
+	
+	for (int i = 0; i < passwords.size();i++)
 	{
+		string path = passwords[i];
 		size_t pos = 0;
-		string path = cyrps[i];
 		std::string token;
-			while ((pos = path.find(delimiter)) != std::string::npos) 
+	
+		while ((pos = path.find(delimiter)) != std::string::npos) 
 		{
-   			token = path.substr(0, pos);
+			token = path.substr(0, pos);
 			tok.push_back(token);
-   			path.erase(0, pos + delimiter.length());
+			path.erase(0, pos + delimiter.length());
 		}
-		//tok.push_back(path);
 	}
-		//tok.shrink_to_fit();
+	
+	int size = passwords.size();
+	string found[size];
+	
+	//cout<<combo.size()<<endl;
+	
+	//#pragma omp parallel for
+	
+	MPI_Init(NULL,NULL);
+	
+	MPI_Comm_size(MPI_COMM_WORLD,&num_processes);
+	MPI_Comm_rank(MPI_COMM_WORLD,&proc_num);
+	
+	for(int i = 0; i < passwords.size(); i ++)
+	{		
+		bool match = false;
+		int j = 0;
+		string t; 
+		string salt = "$6$" + tok[(i + 1)*3 - 1];
 		
-	bool match = false;
-	int j = 0;
-
-	#pragma omp	parallel for
-	for(int i = 0; i < cyrps.size(); i++)
-	{
-		while(match == false && j < combo.size())
-		{
-			string salt  = "$6$" + tok[((i + 1) * 3) - 1];
-			string pass = combo[j];
-			string t = crypt(pass.c_str(),salt.c_str());
-			if(j % 1000 == 0)
-			{
-				cout<<j<<endl;
-			}
-
-			if(t == cyrps[i])
+		
+		for(int j = proc_num; j < combo.size(); j += num_processes)
+		{	 
+			string pass = combo[j]; 
+			t = crypt(pass.c_str(),salt.c_str()); 
+			
+			if(t == passwords[i])
 			{
 				match = true;
-				j--;
+				found[i] = combo[j];
+				j = combo.size() + 1;			
 			}
-			j++;
 		}
 		if(match == true)
 		{
-			cout<<"Password "<<i + 1<<" is "<<combo[j]<< endl;
+			cout<<"Password "<<i + 1<<" is "<<found[i]<<endl;
 		}
-		else
-		{
-			cout<<"Password "<<i + 1<<" Does Not Match "<<combo[j]<< endl;
-		}
-		match = false;
 		j = 0;
-	}
-
+		match = false;
+	}	
+	
+	MPI_Finalize();
 }
